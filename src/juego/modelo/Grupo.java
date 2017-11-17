4787ed4b27ee0f3396d4a604d7d57c6c08f084f4
UtopiaGalega <33326636+UtopiaGalega@users.noreply.github.com>
2017-11-17 11:49:35 +0100

    Update Grupo.java

--- src/juego/modelo/Grupo.java
+++ src/juego/modelo/Grupo.java
@@ -10,24 +10,25 @@ import java.util.ArrayList;
  * 
  */
 
-/**Clase Grupo
+/**
+ * Clase Grupo
  * representa un conjunto de piedras juntas
  */
-
 public class Grupo {
-	/**Se crea un ArrayList  */
+	/** Se crea un ArrayList */
 	private ArrayList celdas = new ArrayList();
 
-	/**Metodo tablero tipo Tablero */
+	/** Método tablero tipo Tablero */
 	private Tablero tablero;
 
-	/**Damos al id el valor 0 */
+	/** Damos al id el valor 0 */
 	private int id = 0;
 
 
-	/** Metodo constructor Grupo
-	 *@param celda celda
-	 *@param tablero tablero
+	/**
+	 * Método constructor Grupo
+	 * @param celda celda
+	 * @param tablero tablero
 	 */
 	public Grupo(Celda celda, Tablero tablero) {
 		this.id = id++;
@@ -36,83 +37,81 @@ public class Grupo {
 
 	}
 
-	/** Metodo obtenerId
-	 * devuelve el identificador del grupo
+	/**
+	 * Método obtenerId
+	 * Cada vez que se instancia un grupo dicho contador debe incrementarse de tal forma que cada grupo tiene un único valor
+	 * Usamos un atributo estático
+	 * el método devuelve el identificador del grupo
+	 *
+	 * @return id
 	 */
-
 	public int obtenerId() {
-		/*
-		 * Cada vez que se instancia un grupo dicho contador debe incrementarse de tal
-		 * forma que cada grupo tiene un único valor. Usamos un atributo estático.
-		 */
-
 		return id;
 	}
 
-
-	/** Metodo obtenerColor
-	 *devuelve el color de las celdas contenidas en un grupo
-	 *@return obtenerColorDePiedra
+	/**
+	 * Método obtenerColor
+	 * devuelve el color de las celdas contenidas en un grupo y null al haber capturado grupo
+	 *
+	 * @return obtenerColorDePiedra
 	 */
-
 	public Color obtenerColor() {
-		/* debe devolver null al haber capturado grupo */
 		Celda celda = (Celda) celdas.get(0);
 		if(celda.estaVacia()){
 		    return null;
-
-        }
-        return celda.obtenerColorDePiedra();
+        	}
+        	return celda.obtenerColorDePiedra();
 	}
 
-
-
-	/** Metodo estaVivo
-	 *devuelve true si los grados de libertad es mayor que cero
+	/**
+	 * Método estaVivo
+	 * devuelve true si los grados de libertad es mayor que cero
 	 */
 	public boolean estaVivo() {
-        int gradoLibertad = 0; //contador
-	    for (int i = 0; i < obtenerTamaño(); i++) {
-            Celda celda = (Celda) celdas.get(i);
-	        gradoLibertad += tablero.obtenerGradosDeLibertad(celda);
-        }
-        if (gradoLibertad != 0){
-	        return true;
-        }
-        return false;
+		int gradoLibertad = 0; //contador
+		for (int i = 0; i < obtenerTamaño(); i++) {
+			Celda celda = (Celda) celdas.get(i);
+	        	gradoLibertad += tablero.obtenerGradosDeLibertad(celda);
+        	}
+        	if (gradoLibertad != 0){
+	        	return true;
+        	}
+        	return false;
 	}
 
-	/** Metodo obtenerTamaño
-	 *tamaño de las celdas
+	/**
+	 * Método obtenerTamaño
+	 * tamaño de las celdas
+	 *
+	 * @return size
 	 */
 	public int obtenerTamaño() {
 		return celdas.size();
-
 	}
 
-
-	/** Metodo contiene
-	 *comprueba si esta contenida una celda en el grupo
+	/**
+	 * Método contiene
+	 * comprueba las coordenadas de la celda para ver si está contenida en el grupo
 	 * @param celda celda
 	 */
 	public boolean contiene(Celda celda) {
-		//comprueba las coordenadas de la celda para ver si está contenida en el grupo
 		return tablero.estaEnTablero(celda);
 	}
 
-
-	/** Metodo añadirCeldas
-	 *añade al grupo el conjunto de celdas pasado como argumento
+	/**
+	 * Método añadirCeldas
+	 * añade al grupo el conjunto de celdas pasado como argumento
 	 * @param grupo grupo
 	 */
 	public void añadirCeldas(Grupo grupo) {
-		for(int i = 0; i < grupo.celdas.size(); i++){
+		for(int i = 0; i < grupo.celdas.size(); i++) {
 			this.celdas.add(grupo.celdas.get(i));
 		}
 	}
 
-	/** Metodo eliminarPiedras
-	 *vacia de piedras el grupo
+	/**
+	 * Método eliminarPiedras
+	 * vacía de piedras el grupo
 	 */
 	public void eliminarPiedras() {
 		for(int i = 0; i < obtenerTamaño(); i++){
@@ -121,16 +120,18 @@ public class Grupo {
 		}
 	}
 
-	/** Metodo generarCopiaEnOtroTablero
-	 *nuevo grupo cuyas celdas son las equivalentes al tablero actual
+	/**
+	 * Método generarCopiaEnOtroTablero
+	 * nuevo grupo cuyas celdas son las equivalentes al tablero actual
+	 *
 	 * @param tablero otrotablero
+	 * @return grupo
 	 */
 	public Grupo generarCopiaEnOtroTablero(Tablero otroTablero) {
 		for (int i = 0; i < celdas.size(); i++) {
 			Celda celda = (Celda) celdas.get(i);
 			Piedra piedra = new Piedra(celda.obtenerColorDePiedra());
 			otroTablero.colocar(piedra, otroTablero.obtenerCeldaConMismasCoordenadas(celda));
-
 		}
 		Celda primeraCelda = (Celda) celdas.get(0);
 		Celda primeraOtroTablero = otroTablero.obtenerCeldaConMismasCoordenadas(primeraCelda);
@@ -143,10 +144,14 @@ public class Grupo {
 		}
 		return grupo;
 	}
-
+	
+	/**
+	 * Método toString
+	 * devuelve el contenido del objeto en formato cadena de caracteres
+	 * 
+	 * @return null
+	 */
 	public String toString() {
-		// devuelve el contenido del objeto en formato cadena de caracteres
-        return null;
+        	return null;
 	}
-
 }
