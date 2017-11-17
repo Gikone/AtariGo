804d6ba22a7833151ea3e33a6e0bfe269ed50fc3
UtopiaGalega <33326636+UtopiaGalega@users.noreply.github.com>
2017-11-17 11:20:34 +0100

    Update Color.java

--- src/juego/modelo/Color.java
+++ src/juego/modelo/Color.java
@@ -8,32 +8,30 @@ package juego.modelo;
  * 
  */
 
-/**clase Color
+/**
+ * clase Color
  * define un color a las piedras
  * @return columna
  */
-
 public enum Color {
 
 	BLANCO('O'), NEGRO('X');
 	private char carácter;
-    /**Metodo constructor
-     * @param  letra
-     * permite ver de que color es cada caracter( O, X)
-     */
+	
+	/**
+	 * Metodo constructor
+	 * @param  letra
+	 * permite ver de que color es cada caracter( O, X)
+	 */
 	private Color(char letra){  //constructor siempre se llama igual que la clase
 		carácter = letra;
 	}
-
-    /**Metodo toChar
-     * @return caracter
-     */
+	
+	/**
+	 * Metodo toChar
+	 * @return caracter
+	 */
 	public char toChar() {
 		return carácter;
 	}
-	
-	/*public static void main(String[] args) {
-		Jugador jugador =
-		
-		*/
 }
